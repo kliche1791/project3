733b98be2fd4602cc0146297fe70cdc97c68dd80
Carlos Castrillon <carloscastrillon1991@gmail.com>
2020-03-26 10:00:23 -0400

    fixing heroku

---
+++ client/build/precache-manifest.39bddd091808c32526ee0585f593e08b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e074d0d2fdd6b31b2fb03c183cbd3d4f",
+    "url": "/static/media/Reddit.e074d0d2.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1b7de43a99dd918f317",
+    "url": "/static/js/main.f76665fc.chunk.js"
+  },
+  {
+    "revision": "d0cfcd264b87065155a2",
+    "url": "/static/js/2.de501a27.chunk.js"
+  },
+  {
+    "revision": "d1b7de43a99dd918f317",
+    "url": "/static/css/main.27a6adf2.chunk.css"
+  },
+  {
+    "revision": "e9ffefb8e84fdfbe342c564876353272",
+    "url": "/index.html"
+  }
+];
